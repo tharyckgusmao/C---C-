db55eea9a1e2985c31c970c5e42e5c60124b4cf7
Rodrigo Lopes <rodrigolmti@gmail.com>
2015-05-10 23:48:32 -0300

    C-C++

--- Lista-Struct/main.cpp
+++ Lista-Struct/main.cpp
@@ -1,10 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int numeroProdutos();
-int produtosEmFalta();
-int cadastroProduto(int quantidade);
-int lisitarProdutos(int quantidade);
+
 
 typedef struct {
 	char descricao[40];
@@ -15,85 +12,74 @@ typedef struct {
 
 
 
-int main()
-{	
-	printf("---- Sistema de Cadastro de Produtos ----");
-	int quantAux;
+void alocaProdutos(produtos *pr, int numero){
 	
-	quantAux = numeroProdutos();
-	cadastroProduto(quantAux);
-	produtosEmFalta();
-	lisitarProdutos(quantAux);
-	
-	system("pause");
-	return 0;
-}
-
-int numeroProdutos(){
-	int aux;
-	printf("\n----- Definindo o tamanho do Stock -----\n");
-	printf("Informe a quantidade de produtos: ");
-	scanf("%d", &aux);
-	
-	produtos *pr; 
-	pr = (produtos *) malloc(aux * sizeof (produtos));
+	pr = (produtos *) malloc(numero * sizeof (produtos));
 	
 	if (!pr){
           printf ("** Erro: Memoria Insuficiente **\n");
-          exit;
+ 
+	produtos *pr;          exit;
       }else{
           printf ("** Memoria Alocada com Sucesso **\n");
-      }    
-	return aux;
+      } 
 }
 
-int cadastroProduto(int quantidade){
-	produtos produtos1[quantidade];
-	int i;
-	int *aux;
-	aux = &pr;
+void preencherProduto(produtos *pr, int numero){
+	int i=0;
 	
-	for(i =0; i <quantidade;i++){
-		printf("\n----- Preenchendo as prateleiras -----", i+1);
+	for(i=0;i<numero;i++){
+		printf("\n----- Preenchendo as prateleiras -----");
 		printf("\nInforme o nome do produto: ");
 		fflush(stdin);
-		gets(*pr[i].descricao);
-		if(*pr[i].descricao == NULL){
-			printf("\nInforme um nome para o produto!");
-		}
+		scanf("%s", &pr[i].descricao);
 		
 		printf("\nInforme o codigo limite 50: ");
 		fflush(stdin);
-		scanf("%d", &produtos1[i].codigo);
-		if(produtos1[i].codigo <0 && produtos1[i].codigo > 50) {
-			printf("\nInforme um codigo valido (0 a 50)!");
-		}
+		scanf("%s", &pr[i]).codigo);
 		
 		printf("\nInforme o preco do produto: ");
 		fflush(stdin);
-		scanf("%d", &produtos1[i].preco);
-		if(produtos1[i].preco < 0) {
-			printf("\nInforme um preco positivo");
-		}
+		scanf("%d", &pr[i].preco);
 		
 		printf("\nInforme a quantidade para stock: ");
 		fflush(stdin);
-		scanf("%d", &produtos1[i].quantidade);
-		if(produtos1[i].quantidade < 0){
-			printf("\nInforme a quantidade positiva!");
-		}
+		scanf("%d", &pr[i].quantidade);
+		
 	}
-	return 0;
 }
 
 int produtosEmFalta() {
 	printf("\nFaltam produtos no stock!\n");
 }
 
-int lisitarProdutos(int quantidade){
-	produtos produtos1[50];
-	int i;
+int lisitarProdutos(produtos *pr){
+	int reg=1;
+	while(pr != NULL) {
+		printf("Log de produto %i", reg++);
+		printf("\tNome %s\n", &pr->descricao);
+		printf("\tCodigo do produto %i\n", &pr->codigo);
+		printf("\tPreco do produto %i\n", &pr->preco);
+		printf("\tQuantidade de produtos %i\n", &pr->quantidade);
+	}	
+}
+
+
+int main(void)
+{	
+	int aux;
+	printf("---- Sistema de Cadastro de Produtos ----");
+	printf("\n----- Definindo o tamanho do Stock -----\n");
+	printf("Informe a quantidade de produtos: ");
+	scanf("%d", &aux);
 	
-	printf("%d", produtos1[50].descricao);
+	produtos *pr;
 	
+	alocaProdutos(pr, aux);
+	preencherProduto(pr, aux);
+	produtosEmFalta();
+	lisitarProdutos(pr);
+	
+	system("pause");
+	return 0;
 }
